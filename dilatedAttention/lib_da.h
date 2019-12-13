71787110b910316759366408bf0984ed75f737af
QiuhaoZhou <57895203+Qiuhao-Zhou@users.noreply.github.com>
2019-12-13 11:10:38 +0800

    Delete lib_da.h

--- dilatedAttention/lib_da.h
+++
@@ -1,5 +0,0 @@
-int da_forward_cuda(const at::Tensor& t, const at::Tensor& *f, at::Tensor& relation);
-int da_backward_cuda(const at::Tensor& dr, const at::Tensor& t, const at::Tensor& f, at::Tensor& dt, at::Tensor& df);
-
-int da_map_forward_cuda(const at::Tensor& relation, const at::Tensor& g, at::Tensor& out);
-int da_map_backward_cuda(const at::Tensor& dout, const at::Tensor& relation, const at::Tensor& g, at::Tensor& dr, at::Tensor& dg);
